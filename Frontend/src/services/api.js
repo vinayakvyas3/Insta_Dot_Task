313e09accbab4cf29c084309d483720b172b3b02
Vinayak Vyas <116144611+vinayakvyas3@users.noreply.github.com>
2025-02-19 13:05:41 +0530

    api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://insta-dot-task-backend-rrkw.onrender.com' });
 
 // Add JWT token to headers
 API.interceptors.request.use((req) => {
