a09d8bc021f4deabe86c6e84510228a8ee018fc4
Vinayak Vyas <116144611+vinayakvyas3@users.noreply.github.com>
2025-02-19 19:47:33 +0530

    db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,8 +2,12 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB Connected');
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error('MongoDB Connection Error:', err.message);
     process.exit(1);
